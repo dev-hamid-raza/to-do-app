6933c70e16adaa3bab24470f4704199cf4c0e401
Hamid Raza <hamidramay2002@gmail.com>
2025-05-14 22:29:53 +0500

    feat: initialize frontend project with React, Vite, and Tailwind CSS
    - Added package.json with dependencies and scripts for development, build, linting, and preview.
    - Included Vite logo SVG in public directory.
    - Created main application component (App.tsx) with a simple "Hello World" message styled with Tailwind CSS.
    - Added React logo SVG in assets directory.
    - Set up global CSS file (index.css) to import Tailwind CSS styles.
    - Configured main entry point (main.tsx) to render the App component.
    - Created TypeScript environment declaration file (vite-env.d.ts).
    - Configured TypeScript for the application with tsconfig.app.json and tsconfig.node.json.
    - Set up Vite configuration file (vite.config.ts) with React and Tailwind CSS plugins.

---
+++ backend/src/index.ts
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv'
+import { app } from './app'
+
+dotenv.config({
+    path: './.env'
+})
+
+app.listen(process.env.PORT, () => {
+    console.log("Server is running at port", process.env.PORT)
+})
